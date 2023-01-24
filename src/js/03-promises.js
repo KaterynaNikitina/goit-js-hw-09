e505dda358f97ad62105de415793cccd74a9cf8b
Katerina <Kateryna.e.nikitina@gmail.com>
2023-01-24 21:14:28 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const submitBtn = document.querySelector('button');
+const delay = document.querySelector('[name="delay"]')
+const step = document.querySelector('[name="step"]')
+const amount = document.querySelector('[name="amount"]')
+
+submitBtn.addEventListener('click', onSubmitBtnClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function onSubmitBtnClick(evt) {
+  evt.preventDefault();
+  let currentDelay = Number(delay.value);
+    for (let position = 1; position <= Number(amount.value); position += 1) {
+      createPromise(position, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+        
+        currentDelay += Number(step.value); 
+    };
+  }
+
+
+ 
