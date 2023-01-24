251039c1de5b740ad851ed36fcab837bd7bc78d4
Katerina <Kateryna.e.nikitina@gmail.com>
2023-01-24 21:35:11 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,6 +5,7 @@ startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click', onStopBtnClick);
 
 function onStartBtnClick(evt) {
+  
   startBtn.setAttribute('disabled', '');
   const isStopBtnActive = stopBtn.hasAttribute('disabled');
 
