6c4af5273cc99fb17a30c47a07763ee38b06192a
KaterynaNikitina <110998098+KaterynaNikitina@users.noreply.github.com>
2023-01-25 08:27:30 +0000

    Deploying to gh-pages from @ KaterynaNikitina/goit-js-hw-09@4be1c81ca138635ef5f5beb5b7aa119348fc1ad8 🚀

---
+++ 01-color-switcher.86288ebe.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function i(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(o){n=setInterval(i,1e3),t.setAttribute("disabled",""),isStopBtnActive=e.hasAttribute("disabled"),isStopBtnActive&&e.removeAttribute("disabled","")})),e.addEventListener("click",(function(i){clearInterval(n),e.setAttribute("disabled",""),t.removeAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.86288ebe.js.map
